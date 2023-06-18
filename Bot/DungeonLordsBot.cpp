749e53cfef121c355ec788ce52abaf72999aa2cf
Kalkom3 <3kalkom3@gmail.com>
2023-06-18 10:17:30 +0200

    Hero test update

--- Bot/DungeonLordsBot.cpp
+++ Bot/DungeonLordsBot.cpp
@@ -1,8 +1,27 @@
 ﻿
 #include <iostream>
+#include "DlLogger.h"
+#include "HeroesTeam.h"
+#include "BattleRound.h"
+#include "TrapCard.h"
 
 int main()
 {
+	HeroesTeam testTeam;
+
+	LoggingConfig::s_printToConsole = true;
+	LoggingConfig::s_logLevel = logLevel::INFO;
+
+	testTeam.AddHero(Hero(2, 1, HeroClass::WARRIOR));
+	testTeam.AddHero(Hero(3, 1, HeroClass::PRIEST));
+	testTeam.AddHero(Hero(1, 1, HeroClass::WARRIOR));
+
+	TrapCard trapCard("Poisoned dart");
+
+	BattleRound testRound(testTeam);
+	testRound.AddTrap(trapCard, 1);
+	testRound.StartBattle();
+	LOG(INFO) << testTeam.GetHero(1).GetHitPoints() << "-" << testTeam.GetHero(1).GetPosioned();
 
 	return 0;
 }
--- GameAPITests/src/HeroTeamTests.cpp
+++ GameAPITests/src/HeroTeamTests.cpp
@@ -1,5 +1,6 @@
 #include "gtest\gtest.h"
 #include "HeroesTeam.h"
+#include "Hero.h"
 #include "GameAPIMock.h"
 
 /// <summary>
@@ -8,9 +9,9 @@
 TEST(HeroTeamTests, HeroPriorityTest) {
 	HeroesTeam testTeam;
 	
-	testTeam.AddHero(std::make_unique<Hero>(2, 1, HeroClass::WARRIOR));
-	testTeam.AddHero(std::make_unique<Hero>(3, 1, HeroClass::PRIEST));
-	testTeam.AddHero(std::make_unique<Hero>(1, 1, HeroClass::WARRIOR));
+	testTeam.AddHero(Hero(2, 1, HeroClass::WARRIOR));
+	testTeam.AddHero(Hero(3, 1, HeroClass::PRIEST));
+	testTeam.AddHero(Hero(1, 1, HeroClass::WARRIOR));
 
 	EXPECT_EQ(testTeam.GetHero(0).GetHitPoints(), 1);
 	EXPECT_EQ(testTeam.GetHero(1).GetHitPoints(), 2);
