43f2b63b1782c764e323033bb6d5dcc65a9129d0
Bartłomiej Łotko <bartlomiej.lotko@gmail.com>
2023-11-07 19:24:10 +0100

    New test added

--- GameAPITests/src/HeroTeamTests.cpp
+++ GameAPITests/src/HeroTeamTests.cpp
@@ -19,3 +19,22 @@ TEST(HeroTeamTests, HeroPriorityTest) {
 	EXPECT_EQ(testTeam.GetHero(2).GetHitPoints(), 3);
 	EXPECT_EQ(testTeam.GetHero(3).GetHitPoints(), 4);
 }
+
+/// <summary>
+/// Test that heroes correctly reposition after death
+/// </summary>
+TEST(HeroTeamTests, HeroReAsingnTest) {
+	HeroesTeam testTeam;
+	
+	testTeam.AddHero(Hero(2, 1, HeroClass::WARRIOR));
+	testTeam.AddHero(Hero(3, 1, HeroClass::PRIEST));
+	testTeam.AddHero(Hero(1, 1, HeroClass::WARRIOR));
+
+	//kill second hero
+	testTeam.GetHero(1).ReceiveDamage(2);
+
+	//check size of team and heroes positions
+	EXPECT_EQ(testTeam.GetTeamSize(), 2);
+	EXPECT_EQ(testTeam.GetHero(0).GetHitPoints(), 1);
+	EXPECT_EQ(testTeam.GetHero(1).GetHitPoints(), 3);
+}
